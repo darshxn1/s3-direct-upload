e1fa9934032d845e6f19dc135cc3e9f301025098
darshxn1 <129256556+darshxn1@users.noreply.github.com>
2023-07-01 13:24:14 +0530

    Update s3.js

--- back/s3.js
+++ back/s3.js
@@ -6,8 +6,8 @@ const randomBytes = promisify(crypto.randomBytes)
 
 dotenv.config()
 
-const region = "us-west-2"
-const bucketName = "direct-upload-s3-bucket-thing"
+const region = "us-east-1"
+const bucketName = "image-upload-bucket11"
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
